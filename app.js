792eac1e893c16ae82b2a2e3ada7de82f7adb3bc
hishshah <ghishshah@yahoo.com>
2017-04-14 15:41:42 +0700

    tampilan lovelist menggunakan list json

--- app.js
+++ app.js
@@ -36,8 +36,47 @@ app.use('/lovelist', lovelist);
 app.post('/', urlencodedParser, function(req, res) {
 	var post = req.body;
 	if (post.username_or_email == 'frontend' && post.password == "intern") {
-		res.render('lovelist', {data: req.body});
-		var token = randtoken.generate(16);
+		var token = randtoken.generate(40);
+    var json =
+    [
+      {
+        photo: "images/vans.png",
+        name: "Vans Disney", 
+        price: "Rp 720.000",
+        comment: "2",
+        loved: "7"
+      },
+      {
+        photo: "images/poster.png",
+        name: "Poster Kayu Tema Vespa", 
+        price: "Rp 175.000",
+        comment: "0",
+        loved: "4"
+      },
+      {
+        photo: "images/preloved.png",
+        name: "Preloved TOPSHOP", 
+        price: "Rp 650.000",
+        comment: "0",
+        loved: "7"
+      },
+      {
+        photo: "images/dk084.png",
+        name: "DK084", 
+        price: "Rp 160.000",
+        comment: "0",
+        loved: "4"
+      },
+      {
+        photo: "images/jaket.png",
+        name: "Jaket Jeans", 
+        price: "Rp 110.000",
+        comment: "0",
+        loved: "4"
+      }
+    ];
+
+    res.render('lovelist', {data: json});
 	} else {
 		res.render('error')
 	}
--- routes/lovelist.js
+++ routes/lovelist.js
@@ -3,7 +3,46 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('lovelist', { title: 'Prelo' });
+	var json =
+		[
+			{
+				photo: "images/vans.png",
+				name: "Vans Disney", 
+				price: "Rp 720.000",
+				comment: "2",
+				loved: "7"
+			},
+			{
+				photo: "images/poster.png",
+				name: "Poster Kayu Tema Vespa", 
+				price: "Rp 175.000",
+				comment: "0",
+				loved: "4"
+			},
+			{
+				photo: "images/preloved.png",
+				name: "Preloved TOPSHOP", 
+				price: "Rp 650.000",
+				comment: "0",
+				loved: "7"
+			},
+			{
+				photo: "images/dk084.png",
+				name: "DK084", 
+				price: "Rp 160.000",
+				comment: "0",
+				loved: "4"
+			},
+			{
+				photo: "images/jaket.png",
+				name: "Jaket Jeans", 
+				price: "Rp 110.000",
+				comment: "0",
+				loved: "4"
+			}
+		];
+		
+	res.render('lovelist', { data: json });
 });
 
 module.exports = router;

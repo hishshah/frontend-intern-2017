e62edd716f83a4a09df846130c5d7e5f9ace442f
hishshah <ghishshah@yahoo.com>
2017-04-14 16:38:39 +0700

    cleaning

--- public/js/login.js
+++ public/js/login.js
@@ -1,37 +1,3 @@
-function validasiUsername() {
-	var usern = document.forms["login"]["usern"].value;
-	
-	if (usern == null || usern == "") {
-		alert("Email atau Username tidak boleh kosong");
-		return false;
-	} else 
-	if (usern.length < 4) {
-		alert("Email atau Username minimal 4 karakter");
-		return false;
-	} else return true;
-}
-
-function validasiPassword() {
-	var passw = document.forms["login"]["pass"].value;
-	
-	if (passw == null || passw == "") {
-		alert("Password tidak boleh kosong");
-		return false;
-	} else 
-	if (passw.length < 6) {
-		alert("Password minimal 6 karakter");
-		return false;
-	} else return true;
-}
-
-function validasi() {
-	var pass = validasiPassword(), user = validasiUsername();
-	
-	if ((pass==true) && (user==true)) {
-		return true;
-	} else return false;
-}
-
 function popupBox() {
 	window.alert('Still in Development');
 }
